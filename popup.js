93377a4f17d89a69b44f4910d6383d2a06b841c9
tomzhu1024 <1184997399@qq.com>
2020-06-14 14:38:56 +0800

    new UI

--- popup.js
+++ popup.js
@@ -7,56 +7,58 @@ class PopupCache {
 }
 
 function iconChange() {
-    if(document.getElementById("blacklistBtn").checked || document.getElementById("switch2").checked ) {
+    if (siteBlocked || document.getElementById("incognito-switch").checked) {
         // send message to background script
-        chrome.runtime.sendMessage({ "newIconPath" : "images/f248.png" });
-    }else{
-        chrome.runtime.sendMessage({ "newIconPath" : "images/f148.png" });
+        chrome.runtime.sendMessage({"newIconPath": "images/f248.png"});
+    } else {
+        chrome.runtime.sendMessage({"newIconPath": "images/f148.png"});
     }
 }
 
 function reloadPopup() {
     console.log(popupCache);
-    var domain = popupCache.domain;
-    document.querySelector("#siteurl").innerHTML = domain;
-    var btnStatus = popupCache.blacklist.has(domain);
-    document.getElementById("blacklistBtn").checked = btnStatus;
-    document.getElementById("switch2").checked = popupCache.config.isIncognito;
+    let domain = popupCache.domain;
+    document.querySelector("#block-site-url").innerHTML = domain;
+    siteBlocked = popupCache.blacklist.has(domain);
+    setBlockSiteState(siteBlocked, false).then();
+    document.getElementById("incognito-switch").checked = popupCache.config.isIncognito;
 }
 
 function toggleIncognito() {
-    var saveBtn = document.getElementById("saveBtn");
-    saveBtn.disabled = false;
-    saveBtn.innerHTML = "Save Changes";
     console.log("toggle incognito");
     // get latest configuration
-    var incognitoStatus = document.getElementById("switch2").checked;
+    let incognitoStatus = document.getElementById("incognito-switch").checked;
     // update local cache object
-    var currTime = (new Date()).toJSON();
+    let currTime = (new Date()).toJSON();
     popupCache.config.isIncognito = incognitoStatus;
     if (incognitoStatus) {
         popupCache.config.startTime = currTime;
     }
     popupCache.config.endTime = currTime;
     port.postMessage(generateRequest("incognito"));
+
+    // auto save
+    saveChange();
 }
 
 function toggleBlacklist() {
-    var saveBtn = document.getElementById("saveBtn");
-    saveBtn.disabled = false;
-    saveBtn.innerHTML = "Save Changes";
     console.log("toggle blacklist");
-    var inBlacklist = document.getElementById("blacklistBtn").checked;
-    var domain = popupCache.domain;
-    if (inBlacklist) {
+    siteBlocked = !siteBlocked;
+    let domain = popupCache.domain;
+    if (siteBlocked) {
+        setBlockSiteState(true, true).then();
         popupCache.blacklist.add(domain);
     } else {
+        setBlockSiteState(false, true).then();
         popupCache.blacklist.delete(domain);
     }
+
+    // auto save
+    saveChange();
 }
 
 function generateRequest(reqType, args) {
-    switch(reqType) {
+    switch (reqType) {
         case "query":
             return {reqType: reqType, keys: args};
         case "update":
@@ -68,31 +70,81 @@ function generateRequest(reqType, args) {
 
 function saveChange() {
     port.postMessage(generateRequest("update"));
-    window.close();
+
+    // change icon
+    iconChange();
+}
+
+async function setBlockSiteState(state, doAnimation) {
+    let animationDuration = 300;
+    let animationEasing = 'linear';
+    if (state) {
+        document.getElementById('btn-block-site').innerText = 'unblock site';
+        document.getElementById('block-site-hint').innerText = 'Store my browse histories from this site';
+        if (doAnimation) {
+            await Promise.all([
+                $('#large-cookie-1').animate({opacity: '0'}, {duration: animationDuration, easing: animationEasing}).promise(),
+                $('#large-cookie-2').animate({opacity: '1'}, {duration: animationDuration, easing: animationEasing}).promise()
+            ]);
+            await Promise.all([
+                $('#large-cookie-2').animate({opacity: '0'}, {duration: animationDuration, easing: animationEasing}).promise(),
+                $('#large-cookie-3').animate({opacity: '1'}, {duration: animationDuration, easing: animationEasing}).promise()
+            ]);
+            await Promise.all([
+                $('#large-cookie-3').animate({opacity: '0'}, {duration: animationDuration, easing: animationEasing}).promise(),
+                $('#large-cookie-4').animate({opacity: '1'}, {duration: animationDuration, easing: animationEasing}).promise()
+            ]);
+        } else {
+            $('#large-cookie-1').css({opacity: '0'});
+            $('#large-cookie-2').css({opacity: '0'});
+            $('#large-cookie-3').css({opacity: '0'});
+            $('#large-cookie-4').css({opacity: '1'});
+        }
+    } else {
+        document.getElementById('btn-block-site').innerText = 'block site';
+        document.getElementById('block-site-hint').innerText = 'Never store my browse histories from this site';
+        if (doAnimation) {
+            await Promise.all([
+                $('#large-cookie-4').animate({opacity: '0'}, {duration: animationDuration, easing: animationEasing}).promise(),
+                $('#large-cookie-3').animate({opacity: '1'}, {duration: animationDuration, easing: animationEasing}).promise()
+            ]);
+            await Promise.all([
+                $('#large-cookie-3').animate({opacity: '0'}, {duration: animationDuration, easing: animationEasing}).promise(),
+                $('#large-cookie-2').animate({opacity: '1'}, {duration: animationDuration, easing: animationEasing}).promise()
+            ]);
+            await Promise.all([
+                $('#large-cookie-2').animate({opacity: '0'}, {duration: animationDuration, easing: animationEasing}).promise(),
+                $('#large-cookie-1').animate({opacity: '1'}, {duration: animationDuration, easing: animationEasing}).promise()
+            ]);
+        } else {
+            $('#large-cookie-1').css({opacity: '1'});
+            $('#large-cookie-2').css({opacity: '0'});
+            $('#large-cookie-3').css({opacity: '0'});
+            $('#large-cookie-4').css({opacity: '0'});
+        }
+    }
 }
 
 console.log("popup: extension starts");
 // blacklist cache
-var popupCache;
-var port = chrome.runtime.connect({name: "popup"});
+let popupCache;
+let siteBlocked = false;
+let port = chrome.runtime.connect({name: "popup"});
 port.postMessage(generateRequest("query", ["config", "blacklist"]));
 
-port.onMessage.addListener(function(msg) {
-    if (msg.resType == "query") {
+port.onMessage.addListener(function (msg) {
+    if (msg.resType === "query") {
         console.log(msg);
-        var blacklist = new Set(msg.values.blacklist);
-        var domainName = msg.domainName;
+        let blacklist = new Set(msg.values.blacklist);
+        let domainName = msg.domainName;
         popupCache = new PopupCache(domainName, blacklist, msg.values.config);
         reloadPopup();
     }
 });
 
 // Add event listeners
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("blacklistBtn").addEventListener("change", toggleBlacklist);
-    document.querySelector("#switch2").addEventListener("change", toggleIncognito);
-    var saveBtn = document.getElementById("saveBtn");
-    saveBtn.disabled = true;
-    saveBtn.innerHTML = "Saved";
-    saveBtn.addEventListener("click", saveChange);
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("btn-block-site").addEventListener("click", toggleBlacklist);
+    document.querySelector("#incognito-switch").addEventListener("change", toggleIncognito);
+    $('img').on('dragstart', () => false);  // make all images not draggable
 });
