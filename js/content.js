48ddce1e10820e107a220c1b568a6489f5dc1040
Szmbbq <github4szmbbq>
2020-07-11 12:40:18 -0400

    fixed invalid context error in content script

--- js/content.js
+++ js/content.js
@@ -1,6 +1,14 @@
-chrome.runtime.sendMessage({reqType: "tabOpen", domain: location.hostname});
+var reconnectToBackground = function() {
+    setTimeout(connectToBackground, 1000);
+}
 
-window.addEventListener("beforeunload", function(event) {
-    const data = performance.getEntriesByType("resource").map(e => e.name);
-    chrome.runtime.sendMessage({reqType: "tabClose", domain: location.hostname, url: location.href, data: data});
-});
+var connectToBackground = function() {
+    chrome.runtime.sendMessage({reqType: "tabOpen", domain: location.hostname});
+
+    window.addEventListener("beforeunload", function(event) {
+        const data = performance.getEntriesByType("resource").map(e => e.name);
+        chrome.runtime.sendMessage({reqType: "tabClose", domain: location.hostname, url: location.href, data: data});
+    });
+}
+
+connectToBackground();
